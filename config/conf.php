963c98690739885c9cd361125f14e7318d4265f0
Digonnet jules <114817002+julesdig@users.noreply.github.com>
2023-11-14 17:40:16 +0100

    Framework with mamp or wamp (#2)

---
+++ config/conf.php
@@ -0,0 +1,13 @@
+<?php
+class conf
+{
+    static $databases = array(
+        'default' => array(
+            'host' => 'localhost',
+            'database' => 'finance',
+            'public' => 'root',
+            'password' => 'root',
+
+        )
+    );
+}
---
+++ views/categorys/index.php
@@ -0,0 +1,24 @@
+<div class="container-fluid !direction !spacing">
+    <div class="row">
+        <div class="col">
+            <br>
+            <h1> Les catégories </h1>
+            <br>
+            <table class="table table-hover  table-sm">
+                <thead>
+                    <tr class="bg-danger">
+                        <th scope="col">identifiant</th>
+                        <th scope="col">Catégorie</th>
+
+                    </tr>
+                </thead>
+
+                <tbody>
+
+            </table>
+            <br>
+
+        </div>
+
+    </div>
+</div>
