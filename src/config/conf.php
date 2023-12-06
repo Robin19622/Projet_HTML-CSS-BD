bddbd6833b7312333350a93ace0c8f2381793956
jules <judigonnet@gmail.com>
2023-12-06 23:53:42 +0100

    documentation sur les fichiers moteurs

--- src/config/conf.php
+++ src/config/conf.php
@@ -1,15 +1,33 @@
 <?php
+/**
+ * Classe conf
+ *
+ * Cette classe contient les paramètres de configuration de l'application.
+ */
 class conf
 {
+    /**
+     * @var string $tags
+     * Une chaîne contenant les balises HTML autorisées dans l'application.
+     */
     public static $tags = "<br><p><i><u><h1><h2><h3><h4><h5><h6><strong><small><b><table><td><th><tr><a><ul><li><ol><tbody><figure></blockquote>";
 
-    static $databases = array(
+    /**
+     * @var array $databases
+     * Un tableau associatif contenant les configurations de base de données.
+     * Chaque clé du tableau représente une connexion de base de données différente.
+     * Chaque valeur est un tableau associatif contenant les clés suivantes :
+     * - 'host': Le nom d'hôte du serveur de base de données.
+     * - 'database': Le nom de la base de données.
+     * - 'public': Le nom d'utilisateur à utiliser pour se connecter à la base de données.
+     * - 'password': Le mot de passe à utiliser pour se connecter à la base de données.
+     */
+    public static $databases = array(
         'default' => array(
             'host' => 'db',
             'database' => 'humanRessource',
             'public' => 'root',
             'password' => 'example',
-
         )
     );
 }
--- src/core/session.php
+++ src/core/session.php
@@ -1,18 +1,28 @@
 <?php
+/**
+ * Classe Session
+ *
+ * Cette classe gère les sessions utilisateur pour l'application. Elle fournit des méthodes pour écrire et lire des données de session, vérifier si un utilisateur est connecté, et gérer les messages flash.
+ */
 class Session
 {
-    public $id;
-    public $table;
-    public $conf = "default";
-    public $db;
-    // read : un select sur clé primaire 
-
+    /**
+     * Le constructeur de la classe. Il démarre une nouvelle session si aucune n'est déjà active.
+     */
     function __construct()
     {
         if (!isset($_SESSION)) {
             session_start();
         }
     }
+
+    /**
+     * Définit un message flash à afficher à l'utilisateur.
+     *
+     * @param string $message Le message à afficher.
+     * @param string $icon L'icône à afficher avec le message.
+     * @param string $type Le type de message (par exemple, "success" ou "error").
+     */
     public function setFlash($message,  $icon = "", $type = "success")
     {
         $_SESSION['flash'] = array(
@@ -21,6 +31,12 @@ class Session
             'type' => $type
         );
     }
+
+    /**
+     * Affiche le message flash actuel, s'il y en a un.
+     *
+     * @return string Le code HTML du message flash.
+     */
     public function flash()
     {
         if (isset($_SESSION['flash']['message'])) {
@@ -34,10 +50,24 @@ class Session
             return $html;
         }
     }
+
+    /**
+     * Écrit une valeur dans la session.
+     *
+     * @param string $key La clé sous laquelle écrire la valeur.
+     * @param mixed $value La valeur à écrire.
+     */
     public function write($key, $value)
     {
         $_SESSION[$key] = $value;
     }
+
+    /**
+     * Lit une valeur de la session.
+     *
+     * @param string|null $key La clé de la valeur à lire. Si null, toutes les données de session sont renvoyées.
+     * @return mixed La valeur lue, ou false si la clé n'est pas définie.
+     */
     public function read($key = null)
     {
         if ($key) {
@@ -50,10 +80,23 @@ class Session
             return $_SESSION;
         }
     }
+
+    /**
+     * Vérifie si un utilisateur est connecté.
+     *
+     * @return bool True si un utilisateur est connecté, false sinon.
+     */
     public function isLogged()
     {
         return isset($_SESSION['User']->id);
     }
+
+    /**
+     * Récupère une propriété de l'utilisateur connecté.
+     *
+     * @param string $key La propriété à récupérer.
+     * @return mixed La valeur de la propriété, ou false si l'utilisateur n'est pas connecté ou si la propriété n'est pas définie.
+     */
     public function user($key)
     {
         if ($this->read('User')) {
