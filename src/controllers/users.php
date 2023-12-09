b284bb9e50dcd39b44b0272703045508b7a4f7d8
Alexis Macle <alexis13macle@gmail.com>
2023-12-09 20:58:44 +0100

    modif page home connecté

--- src/controllers/users.php
+++ src/controllers/users.php
@@ -1,12 +1,29 @@
 <?php
 class users extends controller
 {
+
+    var $employee;
+    var $departement;
+    var $countrie;
+    var $job;
+    var $location;
+
     var mixed $user;
     public function __construct()
     {
         parent::__construct();
         $this->loadModel('user');
+        $this->loadModel('employee');
+        $this->loadModel('departement');
+        $this->loadModel('job');
+        $this->loadModel('countrie');
+        $this->loadModel('location');
         $this->user = $this->models['user'];
+        $this->job = $this->models['job'];
+        $this->employee = $this->models['employee'];
+        $this->departement = $this->models['departement'];
+        $this->countrie = $this->models['countrie'];
+        $this->location = $this->models['location'];
     }
 
     public function index(): void
@@ -27,6 +44,14 @@ class users extends controller
         $this->set($d);
         if ($this->Session->isLogged()) {
             $this->layout = 'admin';
+            $d = [];
+            $d['count_employees'] = $this->employee->getTotalEmployees();
+            $departmentCounts = $this->departement->getDepartmentEmployeeCounts();
+            $d['departmentCounts'] = $departmentCounts;
+            $d['hiresByDate'] = $this->employee->getHiresByDate();
+            $d['count_countries'] = $this->countrie->countAllCountries();
+            $d['count_locations'] = $this->location->countAllLocations();
+            $this->set($d);
             $this->render('loginok');
         } else {
 
